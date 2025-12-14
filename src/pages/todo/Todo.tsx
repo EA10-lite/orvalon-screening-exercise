8318cf8ba353d58c017f234d5c50365bc8728b3e
Emmanuel Anyigor <emanuelanyigor@gmail.com>
2025-12-14 12:43:27 +0100

    working on todo app design

--- src/pages/todo/Todo.tsx
+++ src/pages/todo/Todo.tsx
@@ -30,35 +30,38 @@ const Todo = () => {
 
     return (
         <div className="min-h-screen">
-            <div className="container mx-auto  max-w-[840px] border border-gray-300 dark:border-gray-700 my-8 bg-white h-full">
-                <div className="bg-[#2354A4] p-6">
+            <header>
+                <div className="bg-black p-6">
+                    <h4 className="text-white text-lg font-[500]">Welcome to Todo App</h4>
+                </div>
+                <div className="bg-[#2a2c33] p-6">
                     <div className="space-y-4 mb-4">
                         <form className="w-full" onSubmit={handleSubmit}>
                             <div className="flex items-center">
                                 <input
                                     type="text"
                                     placeholder="Add a new task"
-                                    className="w-full p-3 rounded-lg border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-500"
+                                    className="w-full px-4 py-3 rounded-lg border border-gray-300 text-white placeholder:text-gray-400 focus:outline-none focus:ring-[1.5px] focus:ring-white"
                                     value={task}
                                     onChange={(e) => setTask(e.target.value)}
                                     required
                                 />
                                 <button
                                     type="submit"
-                                    className="ml-2 px-4 py-2 rounded-lg bg-black text-white hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-500 flex items-center justify-center"
+                                    className="ml-2 px-4 py-3 rounded-lg bg-black text-white flex items-center gap-2 justify-center cursor-pointer min-w-32"
                                 >
-                                    <GoPlus className="mr-2" />
-                                    <span className="">Add</span>
+                                    <GoPlus size={18} />
+                                    <span className="uppercase">Add</span>
                                 </button>
                             </div>
                         </form>
                     </div>
 
-                    <div className="flex items-center gap-4">
+                    <div className="flex items-center gap-2">
                         {FILTERS.map((filter) => (
                             <button
                                 key={filter}
-                                className={`px-6 py-2 rounded-lg text-white cursor-pointer ${filter === activeFilter ? "bg-[#1C4383]" : "bg-[#2354A4]"}`}
+                                className={`px-8 py-2 rounded-md text-white cursor-pointer ${filter === activeFilter ? "bg-[#515460]" : "bg-[#515460] opacity-50"}`}
                                 onClick={() => setActiveFilter(filter)}
                             >
                                 <span className="capitalize">{filter}</span>
@@ -66,28 +69,35 @@ const Todo = () => {
                         ))}
                     </div>
                 </div>
+            </header>
 
-                <div className="space-y-4">
-                    {filteredTodos?.length <= 0 ? (
-                        <div className="text-center text-gray-500 dark:text-gray-400 py-12">
-                            <FaRegListAlt className="mx-auto mb-2 text-4xl" />
-                            <p className="text-lg font-medium">No tasks yet</p>
-                            <p className="text-sm">Add a task to get started</p>
-                        </div>
-                    ) : (
-                        <div className="space-y-4">
-                            {todos?.map((todo) => (
-                                <TodoItem
-                                    key={todo.id}
-                                    todo={todo.text}
-                                    handleDelete={() => deleteTodo(todo.id)}
-                                    handleToggle={() => toggleTodo(todo.id)}
-                                    isCompleted={todo.completed}
-                                />
-                            ))}
-                        </div>
-                    )}
-                </div>
+            <div className="space-y-4">
+                {filteredTodos?.length <= 0 ? (
+                    <div className="text-center text-gray-500 dark:text-gray-400 py-12">
+                        <FaRegListAlt className="mx-auto mb-2 text-4xl" />
+                        <p className="text-lg font-medium">
+                            {
+                                activeFilter === "all" ? "No tasks yet" :
+                                activeFilter === "active" ?
+                                "No active tasks" :
+                                "No completed tasks"
+                            }
+                        </p>
+                        <p className="text-sm">Add a task to get started</p>
+                    </div>
+                ) : (
+                    <div className="">
+                        {filteredTodos?.map((todo) => (
+                            <TodoItem
+                                key={todo.id}
+                                todo={todo.text}
+                                handleDelete={() => deleteTodo(todo.id)}
+                                handleToggle={() => toggleTodo(todo.id)}
+                                isCompleted={todo.completed}
+                            />
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     )
@@ -102,14 +112,14 @@ type TodoItemProps = {
 
 const TodoItem = ({ todo, handleDelete, handleToggle, isCompleted }: TodoItemProps) => {
     return (
-        <div className="p-4">
+        <div className="p-6 border-b border-gray-300 last:border-b-0">
             <div className="flex items-center justify-between">
                 <div className="flex items-center gap-2">
                     <button
                         onClick={handleToggle}
-                        className="w-6 h-6 border border-gray-300 dark:border-gray-700 flex items-center justify-center cursor-pointer"
+                        className={`w-6 h-6 border border-gray-300 dark:border-gray-700 flex items-center justify-center cursor-pointer rounded-md ${isCompleted ? "bg-[#515460]" : "bg-transparent"}`}
                     >
-                        {isCompleted && <BsCheck2 size={16} />}
+                        {isCompleted && <BsCheck2 size={16} color="white" />}
                     </button>
                     <p className="text-gray-900 dark:text-gray-100 text-sm font-[500]">{todo}</p>
                 </div>
